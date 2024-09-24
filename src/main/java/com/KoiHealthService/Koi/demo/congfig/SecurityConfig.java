553947a7205f4fb8757e701743d40b745d1bb75f
NguyenVuong-V <vuongtnnse184868@fpt.edu.vn>
2024-09-24 22:39:01 +0700

    Cors Config

---
+++ src/main/java/com/KoiHealthService/Koi/demo/congfig/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.KoiHealthService.Koi.demo.congfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+
+public class SecurityConfig {
+
+
+    @Bean
+    public CorsFilter corsFilter(){
+        //Config Cors
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:5173");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+
+        //Khai báo cors theo từng endpoints
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
