2812c1d6ba87287b6a99efdd99a03b5c24193e7c
nntruong112 <truongnnse184834@fpt.edu.vn>
2024-10-04 07:34:28 +0700

    ready to merge

---
+++ src/test/java/com/KoiHealthService/Koi/demo/service/FishServiceTest.java
@@ -0,0 +1,18 @@
+package com.KoiHealthService.Koi.demo.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+class FishServiceTest {
+
+    @Test
+    void createFish() {
+    }
+}
