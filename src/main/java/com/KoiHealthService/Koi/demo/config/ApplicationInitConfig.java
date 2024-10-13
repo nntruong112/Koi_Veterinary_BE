83b0df5948f78ca8fbfb8feea215395eaf15fda0
NguyenVuong-V <vuongtnnse184868@fpt.edu.vn>
2024-10-13 10:24:35 +0700

    Role Admin

--- src/main/java/com/KoiHealthService/Koi/demo/config/ApplicationInitConfig.java
+++ src/main/java/com/KoiHealthService/Koi/demo/config/ApplicationInitConfig.java
@@ -1,10 +1,9 @@
 package com.KoiHealthService.Koi.demo.config;
 
-import com.KoiHealthService.Koi.demo.Enum.Role;
+
 import com.KoiHealthService.Koi.demo.entity.User;
 import com.KoiHealthService.Koi.demo.repository.UserRepository;
 import lombok.AccessLevel;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +11,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import java.util.HashSet;
 
 @Configuration
 @FieldDefaults(level = AccessLevel.PRIVATE)
