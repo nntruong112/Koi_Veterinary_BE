4499ca4963f93982dca0b9bae51905232baad71b
nntruong112 <truongnnse184834@fpt.edu.vn>
2024-10-13 10:15:27 +0700

    fix userMapper

--- src/main/java/com/KoiHealthService/Koi/demo/mapper/UserMapper.java
+++ src/main/java/com/KoiHealthService/Koi/demo/mapper/UserMapper.java
@@ -1,8 +1,6 @@
 package com.KoiHealthService.Koi.demo.mapper;
-
-
-import com.KoiHealthService.Koi.demo.dto.request.UpdateRequest;
-import com.KoiHealthService.Koi.demo.dto.request.UserRequest;
+import com.KoiHealthService.Koi.demo.dto.request.user.UpdateRequest;
+import com.KoiHealthService.Koi.demo.dto.request.user.UserRequest;
 import com.KoiHealthService.Koi.demo.dto.response.UserResponse;
 import com.KoiHealthService.Koi.demo.entity.User;
 import org.mapstruct.Mapper;
